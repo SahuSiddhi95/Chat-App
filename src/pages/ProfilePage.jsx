9a739b625760222c6eefec649c9755033d2b2f09
SahuSiddhi95 <sahusiddhi95@gmail.com>
2026-03-24 13:02:51 +0530

    first changes

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import assets from '../assets/assets';
 
 function ProfilePage() {
 
-  //  const [selectedImg, setSelectedImg] = useState();
+   const [selectedImg, setSelectedImg] = useState();
+   const navigate = useNavigate();
+   const[name , setName] = useState("Martin Johnson");
+   const [bio , setBio] = useState("Hi Everyone, I am using QuickChat")
   return (
     <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
        <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex
@@ -11,8 +16,11 @@ function ProfilePage() {
          <form action="" className='flex flex-col gap-5 p-10 flex-1'>
              <h3 className='text-lg'>Profile details</h3>
              <label htmlFor="avater" className='flex items-center gap-3 cursor-pointer'>
-              <input type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
-              <img src={""} alt="" />
+              <input onChange={(e)=>setSelectedImg(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+              <img src={selectedImg? URL.createObjectURL(selectedImg) : assets.avatar_icon } 
+              className={`w-12 h-12 ${selectedImg && 'rounded-full'}`}
+              alt="" />
+              upload profile image
              </label>
          </form>
          <img src="" alt="" />
